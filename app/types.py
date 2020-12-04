94d850fdc2ee6bece3132c7f8da2b8482379d8d6
Hiroaki Mikami <hiroaki8270+github@gmail.com>
2020-12-05 08:51:27 +0900

    Train Extractor

--- app/types.py
+++ app/types.py
@@ -1,7 +1,21 @@
-from app.dataset import Dataset
-from app.functions import TokenizeQuery, SplitValue
-from app.valid_dataset import ValidDataset
 from app import charactor_bert
+from app.valid_dataset import ValidDataset
+from app.functions import TokenizeQuery, SplitValue
+from app.dataset import Dataset
+import torch
+from torch import nn
+
+
+def Adam(model: nn.Module, *args, **kwargs):
+    return torch.optim.Optimizer(
+        [
+            {"params": model.decoder.parameters()},
+            {"params": model.encoder.parameters(), "lr": kwargs["lr"] * 0.1},
+        ],
+        *args,
+        **kwargs,
+    )
+
 
 types = {
     "SyntheticDataset": Dataset,
@@ -10,4 +24,5 @@ types = {
     "SplitValue": SplitValue,
     "character_bert.EncodeQuery": charactor_bert.EncodeQuery,
     "character_bert.Extractor": charactor_bert.Extractor,
+    "Adam": Adam
 }
